943932c619172178cbd0f9126c480975920c27ab
SantiagoEsc1216 <santiagoesc1216@gmail.com>
2023-02-10 17:02:08 -0600

    Asistencia y calificaciones

--- Asistencia.cpp
+++ Asistencia.cpp
@@ -8,11 +8,14 @@ class Alumno{
 private:
     string nombre, examen;
     int asistencia;
+    vector<float>calificaciones;
+    vector<float>promedio;
 
 public:
     Alumno(string &_nombre){
         nombre = _nombre;
         asistencia = 0;
+
         examen = "Pendiente";
     }
 
@@ -26,32 +29,50 @@ public:
     int get_asistencia(){
         return asistencia;
     }
+
+    void set_cal(float cal){
+        this->calificaciones.push_back(cal);
+    }
+
+    float get_cal(int parcial){
+        return this->calificaciones[parcial];
+    }
 };
 
 class Grupo{
 private:
     vector<Alumno> alumnos;
-    int tam;
+    string nombre;
+    int tam, parciales, parcial_actual;
 public:
-    void crear_grupo(){
-        string nombre;
 
-        cout<<"Ingresar numero de alumnos"<<endl;
+    Grupo(){
+        this->parcial_actual = 1;
+    }
+
+    void crear_grupo(){
+        string nombre_alumno;
+        cout<<"Nombre del grupo: ";
+        cin>> this->nombre;
+        cout<<"Cantidad de parciales: ";
+        cin>> parciales;
+
+        cout<<"Ingresar numero de alumnos: ";
         cin>>tam;
         for(int i = 0; i < tam; i++){
-            cout<<"Ingresar nombre del alumno "<<i+1<<":";
-            cin>>nombre;
-            Alumno alumno = Alumno(nombre);
+            cout<<"Ingresar nombre del alumno "<<i+1<<": ";
+            cin >> nombre_alumno;
+            Alumno alumno = Alumno(nombre_alumno);
             alumnos.push_back(alumno);
-
         }
+
     }
 
     void tomar_asistencia(){
         cout<<"Toma de asistencia (1.Presente 0.Ausente)"<<endl;
         for(int i = 0; i < tam; i++){
             int presente;
-            cout<<alumnos[i].get_nombre()<<" presente?"<<endl;
+            cout<<alumnos[i].get_nombre()<<" presente?: ";
             cin>> presente;
 
             if(presente){
@@ -59,19 +80,45 @@ public:
             }
         }
     }
+
+    void calificar(){
+        int cal;
+        if(this->parcial_actual <= this->parciales){
+            for(int i = 0; i<tam; i++){
+                cout<<"Parcial: "<<this->parcial_actual<<endl;
+                cout<<"Calificacion de "<<alumnos[i].get_nombre()<<": ";
+                cin>>cal;
+                alumnos[i].set_cal(cal);
+            }
+            this->parcial_actual++;
+        }else{
+            //Extraordinario o titulo
+        }
+
+    }
+
     void imprimir_grupo(){
         for(int i = 0; i < tam; i++){
             cout<<alumnos[i].get_nombre()<<": "<<alumnos[i].get_asistencia()<<endl;
+            cout<<"Calificaciones: ";
+            for(int j = 1; j < this->parcial_actual; j++ ){
+                cout<<alumnos[i].get_cal(j)<<" | ";
+            }
+            cout<<endl;
         }
     }
 };
 
+
+
 int main() {
 
-    Grupo grupo = Grupo();
+    Grupo grupo;
     grupo.crear_grupo();
-    grupo.imprimir_grupo();
     grupo.tomar_asistencia();
+    grupo.calificar();
+    grupo.imprimir_grupo();
+    grupo.calificar();
     grupo.imprimir_grupo();
 }
 
